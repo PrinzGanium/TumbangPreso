2f018fa23f5bbd337fcfc8bb90be7a4b7001f9ad
PrinzGanium <prinzromeogan@gmail.com>
2025-03-24 14:28:47 +0800

    gitignore

--- MenuNavigation/Games/TumbangPreso/player.gd
+++ MenuNavigation/Games/TumbangPreso/player.gd
@@ -54,8 +54,8 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
 
 	speed = WALK_SPEED
 	if Input.is_action_pressed("sprint") and is_on_floor():
